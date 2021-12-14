a5e9e1a09674e1afe766c7b986529d3401dff9c7
cmmerritt <christiane.merritt@gmail.com>
2021-12-13 21:06:41 -0800

    add countdown functionality

--- 01_Pomodoro/pomodoro.js
+++ 01_Pomodoro/pomodoro.js
@@ -1,24 +1,40 @@
-// clicking on gear needs to allow you to select minutes/seconds
-// need to see time change -- minutes decrement until zero and seconds decrement every second 
-// how to implement?
-// setTimeout function?
-// when timer ends (time = 0), color of circle changes from green to red and alert message pops up 
+let time = 65;
+setInterval(() => {
+  if(time > 0) {
+    time -= 1;
+    const minutes = document.getElementsByClassName("minutes")[0];
+    minutes.children[0].value = `${Math.floor(time / 60)}`;
+    const seconds = document.getElementsByClassName("seconds")[0];
+    seconds.children[0].value = `${time % 60}`;
+  } 
+}, 1000);
 
 const startButton = document.getElementsByClassName("start")[0];
-
 const ring = document.getElementsByClassName("ring")[0];
 
 startButton.addEventListener('click', () => {
-  startButton.textContent = startButton.textContent === "start" ? "stop" : "start";
-    if(ring.classList.contains("ending")) {
-      ring.classList.remove("ending");
-    } else {
-    ring.classList.add("ending");
-    };
+  if(startButton.textContent === "start") {
+    startButton.textContent = "stop";
+  } else {
+    startButton.textContent = "start";
+  }
 });
 
-// change ring color from green to red
-// add already existing red ending circle css class to document class
+
+  // startButton.textContent = startButton.textContent === "start" ? "stop" : "start";
+  //   if(ring.classList.contains("ending")) {
+  //     ring.classList.remove("ending");
+  //   } else {
+  //   ring.classList.add("ending");
+  //   };
+
+// events that will have to occur when we run timer
+// - clicking start/stop button
+// - clicking settings
+// - selecting from settings
+// - changing value of minutes
+// - changing value of seconds
+// set variable equal to time and make sure it hits zero (so we don't have to keep track of minutes and seconds separately)
 
 
 
